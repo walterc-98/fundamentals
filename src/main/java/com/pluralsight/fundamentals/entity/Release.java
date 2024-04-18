93c3e5efc77e9035454054497d6a710928e46677
walterc98 <walter.condori.10@gmail.com>
2024-04-17 23:54:01 -0400

    3rd Commit: Adding entities that include Application.java, Release.java, and Ticket.java

---
+++ src/main/java/com/pluralsight/fundamentals/entity/Release.java
@@ -0,0 +1,49 @@
+package com.pluralsight.fundamentals.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Release {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String releaseDate;
+    private String description;
+
+    public Release(){
+    }
+
+    public Release(Integer id, String description, String releaseDate){
+        this.id = id;
+        this.description = description;
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
